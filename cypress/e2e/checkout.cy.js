c76f4e520c13e3de5f77d3e9e3344107db18784c
RotuaTampubolon <@rotuaimmanuela@gmail.com>
2026-05-01 17:24:15 +0700

    feat: add fixture files for data-driven testing

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -2,71 +2,74 @@
 // TEST SUITE: Checkout Feature
 // App: SauceDemo
 // Author: Rotua Immanuela Tampubolon
+// Uses: cy.fixture() for data-driven testing
 // ============================================================
-
-const loginAndAddToCart = () => {
-  cy.visit('/')
-  cy.get('#user-name').type('standard_user')
-  cy.get('#password').type('secret_sauce')
-  cy.get('#login-button').click()
-  cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-  cy.get('.shopping_cart_link').click()
-}
 
 describe('Checkout Feature', () => {
 
   beforeEach(() => {
-    loginAndAddToCart()
+    cy.visit('/')
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_link').click()
   })
 
+  // POSITIVE CASE
   it('TC-009 | Complete checkout with valid data should show confirmation', () => {
-    cy.get('[data-test="checkout"]').click()
-    cy.get('[data-test="firstName"]').type('Rotua')
-    cy.get('[data-test="lastName"]').type('Tampubolon')
-    cy.get('[data-test="postalCode"]').type('20211')
-    cy.get('[data-test="continue"]').click()
-    cy.get('[data-test="finish"]').click()
+    cy.fixture('checkout').then((data) => {
+      cy.get('[data-test="checkout"]').click()
+      cy.get('[data-test="firstName"]').type(data.validCustomer.firstName)
+      cy.get('[data-test="lastName"]').type(data.validCustomer.lastName)
+      cy.get('[data-test="postalCode"]').type(data.validCustomer.postalCode)
+      cy.get('[data-test="continue"]').click()
+      cy.get('[data-test="finish"]').click()
 
-    cy.get('.complete-header')
-      .should('have.text', 'Thank you for your order!')
-
-    cy.screenshot('TC-009-checkout-success-confirmation')
+      cy.get('.complete-header')
+        .should('have.text', 'Thank you for your order!')
+      cy.screenshot('TC-009-checkout-success-confirmation')
+    })
   })
 
+  // NEGATIVE CASES
   it('TC-010 | Empty first name should show validation error', () => {
-    cy.get('[data-test="checkout"]').click()
-    cy.get('[data-test="lastName"]').type('Tampubolon')
-    cy.get('[data-test="postalCode"]').type('20211')
-    cy.get('[data-test="continue"]').click()
+    cy.fixture('checkout').then((data) => {
+      cy.get('[data-test="checkout"]').click()
+      cy.get('[data-test="lastName"]').type(data.missingFirstName.lastName)
+      cy.get('[data-test="postalCode"]').type(data.missingFirstName.postalCode)
+      cy.get('[data-test="continue"]').click()
 
-    cy.get('[data-test="error"]')
-      .should('contain', 'First Name is required')
-
-    cy.screenshot('TC-010-checkout-empty-firstname-error')
+      cy.get('[data-test="error"]')
+        .should('contain', 'First Name is required')
+      cy.screenshot('TC-010-checkout-empty-firstname-error')
+    })
   })
 
   it('TC-011 | Empty last name should show validation error', () => {
-    cy.get('[data-test="checkout"]').click()
-    cy.get('[data-test="firstName"]').type('Rotua')
-    cy.get('[data-test="postalCode"]').type('20211')
-    cy.get('[data-test="continue"]').click()
+    cy.fixture('checkout').then((data) => {
+      cy.get('[data-test="checkout"]').click()
+      cy.get('[data-test="firstName"]').type(data.missingLastName.firstName)
+      cy.get('[data-test="postalCode"]').type(data.missingLastName.postalCode)
+      cy.get('[data-test="continue"]').click()
 
-    cy.get('[data-test="error"]')
-      .should('contain', 'Last Name is required')
-
-    cy.screenshot('TC-011-checkout-empty-lastname-error')
+      cy.get('[data-test="error"]')
+        .should('contain', 'Last Name is required')
+      cy.screenshot('TC-011-checkout-empty-lastname-error')
+    })
   })
 
   it('TC-012 | Empty postal code should show validation error', () => {
-    cy.get('[data-test="checkout"]').click()
-    cy.get('[data-test="firstName"]').type('Rotua')
-    cy.get('[data-test="lastName"]').type('Tampubolon')
-    cy.get('[data-test="continue"]').click()
+    cy.fixture('checkout').then((data) => {
+      cy.get('[data-test="checkout"]').click()
+      cy.get('[data-test="firstName"]').type(data.missingPostalCode.firstName)
+      cy.get('[data-test="lastName"]').type(data.missingPostalCode.lastName)
+      cy.get('[data-test="continue"]').click()
 
-    cy.get('[data-test="error"]')
-      .should('contain', 'Postal Code is required')
-
-    cy.screenshot('TC-012-checkout-empty-postalcode-error')
+      cy.get('[data-test="error"]')
+        .should('contain', 'Postal Code is required')
+      cy.screenshot('TC-012-checkout-empty-postalcode-error')
+    })
   })
 
 })
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,6 +2,7 @@
 // TEST SUITE: Login Feature
 // App: SauceDemo
 // Author: Rotua Immanuela Tampubolon
+// Uses: cy.fixture() for data-driven testing
 // ============================================================
 
 describe('Login Feature', () => {
@@ -10,30 +11,31 @@ describe('Login Feature', () => {
     cy.visit('/')
   })
 
-  // POSITIVE CASES
+  // POSITIVE CASE
   it('TC-001 | Valid login should redirect to Products page', () => {
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    cy.fixture('users').then((users) => {
+      cy.get('#user-name').type(users.validUser.username)
+      cy.get('#password').type(users.validUser.password)
+      cy.get('#login-button').click()
 
-    cy.url().should('include', '/inventory')
-    cy.get('.title').should('have.text', 'Products')
-
-    // Ambil screenshot bukti halaman Products terbuka
-    cy.screenshot('TC-001-login-valid-success')
+      cy.url().should('include', '/inventory')
+      cy.get('.title').should('have.text', 'Products')
+      cy.screenshot('TC-001-login-valid-success')
+    })
   })
 
-  // NEGATIVE CASES
+  //  NEGATIVE CASES
   it('TC-002 | Locked out user should see error message', () => {
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('#password').type('secret_sauce')
-    cy.get('#login-button').click()
+    cy.fixture('users').then((users) => {
+      cy.get('#user-name').type(users.lockedUser.username)
+      cy.get('#password').type(users.lockedUser.password)
+      cy.get('#login-button').click()
 
-    cy.get('[data-test="error"]')
-      .should('be.visible')
-      .and('contain', 'Sorry, this user has been locked out')
-
-    cy.screenshot('TC-002-login-locked-out-error')
+      cy.get('[data-test="error"]')
+        .should('be.visible')
+        .and('contain', 'Sorry, this user has been locked out')
+      cy.screenshot('TC-002-login-locked-out-error')
+    })
   })
 
   it('TC-003 | Empty username should show validation error', () => {
@@ -42,7 +44,6 @@ describe('Login Feature', () => {
 
     cy.get('[data-test="error"]')
       .should('contain', 'Username is required')
-
     cy.screenshot('TC-003-login-empty-username-error')
   })
 
@@ -52,19 +53,19 @@ describe('Login Feature', () => {
 
     cy.get('[data-test="error"]')
       .should('contain', 'Password is required')
-
     cy.screenshot('TC-004-login-empty-password-error')
   })
 
   it('TC-005 | Wrong password should show credentials error', () => {
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('wrongpassword')
-    cy.get('#login-button').click()
+    cy.fixture('users').then((users) => {
+      cy.get('#user-name').type(users.invalidUser.username)
+      cy.get('#password').type(users.invalidUser.password)
+      cy.get('#login-button').click()
 
-    cy.get('[data-test="error"]')
-      .should('contain', 'Username and password do not match')
-
-    cy.screenshot('TC-005-login-wrong-credentials-error')
+      cy.get('[data-test="error"]')
+        .should('contain', 'Username and password do not match')
+      cy.screenshot('TC-005-login-wrong-credentials-error')
+    })
   })
 
 })
